83760e2313c4becc675a0155430dd021d2980384
wws2058 <57310877+wws2058@users.noreply.github.com>
2024-11-27 21:37:54 +0800

    feat: api audit log
    Former-commit-id: e347ba2df3c7345c46fc0f1041769169481c8427

--- middleware/limiter.go
+++ middleware/limiter.go
@@ -3,6 +3,7 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 // api limiter, prevent burst requests
+// TODO
 func ApiLimiter() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
